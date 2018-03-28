9ed5f29963948bb064f295d8d65740ead544815b
Chelsi Jones <cajones.bz@gmail.com>
2018-03-28 06:12:18 -0500

    Establish User model/controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,26 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def log_in(user)
+    session[:user_id] = user.id
+    current_user
+  end
+
+  def current_user=(user)
+    user ? user : nil
+  end
+
+  def current_user
+    current_user ||= current_user=(User.find_by(id: session[:user_id]))
+    return current_user
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  #resources :sessions
+  # home/root can be set with events controller (index)
+  resources :users
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
